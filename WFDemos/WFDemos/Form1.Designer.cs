bf7d8f8ecafbdf1b655b051c5e5cc48f529a64e7
kosta.kiryazov@bmsys.eu <kosta.kiryazov@gmail.com>
2023-04-19 10:32:42 +0300

    Add display double values into chart

---
+++ WFDemos/WFDemos/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WFDemos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.countryComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pricesChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pricesChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // countryComboBox
+            // 
+            this.countryComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.countryComboBox.FormattingEnabled = true;
+            this.countryComboBox.Location = new System.Drawing.Point(116, 29);
+            this.countryComboBox.Name = "countryComboBox";
+            this.countryComboBox.Size = new System.Drawing.Size(307, 21);
+            this.countryComboBox.TabIndex = 0;
+            this.countryComboBox.SelectedIndexChanged += new System.EventHandler(this.countryComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Country";
+            // 
+            // pricesChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.pricesChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.pricesChart.Legends.Add(legend1);
+            this.pricesChart.Location = new System.Drawing.Point(23, 81);
+            this.pricesChart.Name = "pricesChart";
+            this.pricesChart.Size = new System.Drawing.Size(748, 332);
+            this.pricesChart.TabIndex = 2;
+            this.pricesChart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pricesChart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.countryComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pricesChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox countryComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart pricesChart;
+    }
+}
+
---
+++ WFDemos/WFDemos/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using WFDemos.Models;
+
+namespace WFDemos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.countryComboBox.DisplayMember= "Name";
+            this.countryComboBox.ValueMember= "Id";
+            var countries = this.GetCountries();
+            foreach (var country in countries)
+            {
+                this.countryComboBox.Items.Add(country);
+            }
+        }
+
+        private IEnumerable<Country> GetCountries()
+        {
+            return new List<Country>() {
+                new Country{ Id =1, Name = "Bulgaria" },
+                new Country{ Id =2, Name = "Russia" },
+                new Country{ Id =3, Name = "Australia" },
+                new Country{ Id =4, Name = "USA" },
+                new Country{ Id =5, Name = "Great Britan" }
+                };
+        }
+
+        private IEnumerable<double> GetPrices(int countryId)
+        {
+            return new List<double>() { 1.12, 1.33, 1.62, 1.40, 1.08, 1.05, 0.78, 1.06, 1.05, 1.40, 1.72, 1.25 };
+        }
+
+        private void countryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var cmb = (ComboBox)sender;
+            int countryIndex = cmb.SelectedIndex;
+            var countryName = ((Country)cmb.SelectedItem).Name;
+
+            var prices = this.GetPrices(countryIndex);
+
+            var chart = pricesChart.ChartAreas[0];
+            chart.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chart.AxisX.Minimum = 1;
+            chart.AxisX.Maximum = prices.Count();
+
+            chart.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chart.AxisY.Minimum = prices.Min() - 2;
+            chart.AxisY.Maximum = prices.Max() + 2;
+
+            this.pricesChart.Series.Clear();
+
+            pricesChart.Series.Add(countryName);
+            pricesChart.Series[countryName].Color = Color.Blue;
+            pricesChart.Series[countryName].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            foreach (var price in prices)
+            {
+                pricesChart.Series[countryName].Points.AddY(price);
+            }
+        }
+    }
+}
